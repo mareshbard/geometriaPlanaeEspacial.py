562f94b7302cfe1ff46ccd2371b6d5822c1a62bc
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2023-04-25 10:14:24 -0300

    Create Paralelepipedo.py

---
+++ Espaciais/Prismas/volumePrismas/Paralelepipedo.py
@@ -0,0 +1,5 @@
+altura = int(input("Digite altura: \n"))
+larg = int(input("Digite a largura: \n"))
+comp = int(input("Digite o comprimento: \n"))
+area = altura * larg * comp
+print("A área do seu prisma é: ",area)
