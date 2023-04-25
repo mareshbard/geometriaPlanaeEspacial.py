44ca003d232554ff67b73b8b778f7f6234662dc7
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2023-04-25 09:56:43 -0300

    Create areaTrap.py

---
+++ Trapezio/areaTrap.py
@@ -0,0 +1,5 @@
+diagmenor = int(input("Digite a diagonal menor: \n"))
+diagmaior = int(input("Digite a diagonal maior: \n"))
+altura = int(input("Digite a altura: \n"))
+areaTrap = (diagmenor * diagmaior * altura) /2
+print("A área do trapézio: ", areaTrap)
