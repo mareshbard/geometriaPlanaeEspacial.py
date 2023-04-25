90bf4b655bb21771364c0ad91577e603bdb9406e
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2023-04-25 09:35:48 -0300

    Update areaTE.py

--- Triangulo/Equilatero/areaTE.py
+++ Triangulo/Equilatero/areaTE.py
@@ -1,5 +1,5 @@
 import math
 lado = int(input("Digite o lado do seu triângulo: \n"))
 raiz3 = math.sqrt(3)
-area = ((lado ** 2) * raiz3) /2
+area = ((lado ** 2) * raiz3) /4
 print("A área do seu triangulo é: ", area)
